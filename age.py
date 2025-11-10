d8ec3b96eb9d3b2b45bcde112c2747f8ce50c017
prajwalsm <prajwalmenasinakayi@gmail.com>
2025-11-10 12:28:26 +0530

    Create age.py

---
+++ age.py
@@ -0,0 +1,8 @@
+try:
+  age=int("enter your age")
+  if(age>=18)
+  print("you are eligible to vote")
+  else (age<18)
+  print("you are not eligible to vote")
+  expect valueerror
+  print (" enter a valid integer for age")
